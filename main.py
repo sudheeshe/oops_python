e7b01ad2a54b278467b4fe7b41f92f7f1d15b135
Sudheesh E <sudheeshedathala@gmail.com>
2022-06-08 01:21:22 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from print_template import template
+
+class Parent_1:
+
+    def __init__(self, name= "John", age = 44, gender='Male'):
+        self.name = name # public variable
+        self._age = age # private variable
+        self.__gender = gender # protected variable
+
+    def parent_1_method(self):
+        print("\t\t'Parent_1' class method is accessing through 'Child' class")
+
+
+
+class Parent_2:
+    def __init__(self, name= "Alice", age = 39, gender='female'):
+        self.name = name # public variable
+        self._age = age # private variable
+        self.__gender = gender # protected variable
+
+    def parent_2_method(self):
+        print("\t\t'Parent_2' class method is accessing through 'Child' class")
+
+
+
+class Child(Parent_1, Parent_2):
+    def __init__(self):
+        super().__init__()
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        @template
+        def demo():
+            a = Child()
+            return a
+
+    except Exception as e:
+        raise e
+
+
---
+++ print_template.py
@@ -0,0 +1,21 @@
+# Creating a function to print in a template
+
+def template(func):
+
+    def format():
+        print("\n#####################################################################################")
+        print("\t\t\t\t\tUSING DECORATORS FOR PRINTING THE INFO\t\t")
+        print("#####################################################################################")
+
+        print("\n")
+        print("Demo of Abstraction : ")
+        print(f'\t\tPrinting the "PUBLIC_VARIABLE" of Parent_1  using Child              : {func().name}')
+        print(f'\t\tPrinting the "PRIVATE_VARIABLE" of Parent_1  using Child             : {func()._age}')
+        print(f'\t\tPrinting the "PROTECTED_VARIABLE" of Parent_1  using Child           : {func()._Parent_1__gender}')
+        print("#####################################################################################\n")
+        print("Demo of Multiple Inhertence  : ")
+        print("\t\tThe 'Child' is Inheriting both 'Parent_1' and 'Parent_2' classes")
+        func().parent_1_method()
+        func().parent_2_method()
+
+    return format()
